7ce88140dbead284b4ca4da3ee6292ee7fb92945
serafinaliem <christabelle.serafina.stu@elyon.sch.id>
2021-11-26 14:43:49 +0700

    New updates for Heroku deployment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c%_(@fqjlo1_kv*)or@l3ld@ps4*(vz0(#*kk6x1_=@9g9p(f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
